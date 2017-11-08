be5a6bf359252aa316f431418357b716c78d5910
Péter Kalicz <kaliczp@gmail.com>
2017-11-08 11:33:41 +0100

    Summary

--- dftest.R
+++ dftest.R
@@ -1,4 +1,8 @@
 ## Read in temperature and precipitation data and create a data.frame
 Temp<-scan("t.txt")
 Prec <- scan("p.txt")
-data.frame(Precipitation = Prec, Temperature = Temp)
+Meteo.df <- data.frame(Precipitation = Prec, Temperature = Temp)
+rm(Prec);rm(Temp)
+summary(c(1:100,100000)) # summary on vector
+summary(Meteo.df)
+summary(factor(Meteo.df[,1] > 0,labels =c("NoPrec","Prec")))
